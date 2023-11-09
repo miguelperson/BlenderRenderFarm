1c3f6bcf8e6b2d17385381b0c27ea0bb4847e2eb
Victoria Ten <victoria.ten.work@gmail.com>
2023-11-09 09:37:21 -0500

    old and new thread files

--- thread_test.py
+++ thread_test.py
@@ -1,26 +1,38 @@
 import threading
+import time
+import icecream as ic
 
 frames = 10
 q1 = True
 q2 = True
- 
-def print_cube(num):
-    # function to print cube of given num
-    print("Cube: {}" .format(num * num * num))
- 
- 
-def print_square(num):
-    # function to print square of given num
-    print("Square: {}" .format(num * num))
- 
+rendered = 0
+
+def move_frame():
+    rendered = input("Finished frame: ")
+    return
+
+def queue1(num):
+    return
+
+def queue2(num):
+    return
+
+rendered_frames = threading.Thread(target=move_frame, daemon=True)    
+
 i=1
 while (i<=frames):
+    time.sleep(1)
     if (q1==True):
-        t1 = threading.Thread(target=print_square, args=(i,))
+        t1 = threading.Thread(target=queue1, daemon=True, args=(i,))
+        q1 = False
         i += 1
     if (q2==True):
-        t2 = threading.Thread(target=print_cube, args=(i,))
+        t2 = threading.Thread(target=queue2, daemon=True, args=(i,))
+        q2 = False
         i += 1
+    else:
+        ic(f"Queue 1: {i}")
+        ic(f"Queue 2: {i}")
  
     # starting thread 1
     t1.start()
