09555d3396630a6c31e38853de7f0f82d0f74c59
miguelperson <mi529437@ucf.edu>
2023-11-28 14:40:16 -0500

    working more on the connection handling for the socket code

--- SocketConnection.py
+++ SocketConnection.py
@@ -17,7 +17,19 @@ for i in range(totalClient): # connecting ricardos computer to the server
     conn = sock.accept() # accepts socket connection with clients
     connections.apend(conn) # appending connection to array for the sake of storage and shit
     print('connected with the client', i+1) # console confirmation of the client being conneced (Ricardo)
+
+fileno = 0
+idx = 0
+for conn in connections:
+    # recieving file data
+    idx += 1
+    data = conn[0].recv(1024).decode
+    if not data: 
+        continue
+    filename = 'output'+str(fileno)+'.txt'
+    fileno = fileno+1
     
 
+# creating a new file at server end and writig the data
 def connection():
     print('placeholder')
