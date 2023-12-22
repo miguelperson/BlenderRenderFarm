038816d9f69a6e3339600c3b3a82ba431f87a1d9
Victoria Ten <victoria.ten.work@gmail.com>
2023-12-22 10:50:47 -0500

    simplifide manipulateDB

--- callDB.py
+++ callDB.py
@@ -5,6 +5,12 @@ client = "ClientA"
 frames_total = 1000
 start_frame = 1
 end_frame = 1000
+projectID = 3
 
 # Send gathered information to the database
-insert_into_project(project_name, client, frames_total, start_frame, end_frame)
+
+#insert_into_project(project_name, client, frames_total, start_frame, end_frame)
+#insert_into_workers(worker, available, projectID)
+#insert_into_render(frame_number, projectID)
+
+remove_from_project(projectID,)
--- manipulateDB.py
+++ manipulateDB.py
@@ -8,13 +8,10 @@ def connect_to_db():
         database="renderdb"
     )
 
-def insert_into_project(project_name, client, frames_total, start_frame, end_frame):
+def sql_exe(sql,val):
     try:
         mydb = connect_to_db()
         mycursor = mydb.cursor()
-        
-        sql = "INSERT INTO project (project_name, client, frames_total, start_frame, end_frame) VALUES (%s, %s, %s, %s, %s)"
-        val = (project_name, client, frames_total, start_frame, end_frame)
         
         mycursor.execute(sql, val)
         
@@ -24,34 +21,26 @@ def insert_into_project(project_name, client, frames_total, start_frame, end_fra
     except mysql.connector.Error as err:
         print("Error:", err)
 
+
+def insert_into_project(project_name, client, frames_total, start_frame, end_frame):
+    sql = "INSERT INTO project (project_name, client, frames_total, start_frame, end_frame) VALUES (%s, %s, %s, %s, %s)"
+    val = (project_name, client, frames_total, start_frame, end_frame)
+    sql_exe(sql,val)
+    
+        
 def insert_into_workers(worker, available, projectID):
-    try:
-        mydb = connect_to_db()
-        mycursor = mydb.cursor()
-        
-        sql = "INSERT INTO workers (worker, available, projectID) VALUES (%s, %s, %s)"
-        val = (worker, available, projectID)
-        
-        mycursor.execute(sql, val)
-        
-        mydb.commit()
-        mycursor.close()
-        mydb.close()
-    except mysql.connector.Error as err:
-        print("Error:", err)
+    sql = "INSERT INTO workers (worker, available, projectID) VALUES (%s, %s, %s)"
+    val = (worker, available, projectID)
+    sql_exe(sql,val)
 
 def insert_into_render(frame_number, projectID):
-    try:
-        mydb = connect_to_db()
-        mycursor = mydb.cursor()
+    sql = "INSERT INTO workers (frame_number, projectID) VALUES (%s, %s)"
+    val = (frame_number, projectID)
+    sql_exe(sql,val)
         
-        sql = "INSERT INTO workers (frame_number, projectID) VALUES (%s, %s)"
-        val = (frame_number, projectID)
+
+def remove_from_project(projectID,):
+    sql = "DELETE FROM project WHERE projectID = %s"
+    val = (projectID,)  
+    sql_exe(sql,val)
         
-        mycursor.execute(sql, val)
-        
-        mydb.commit()
-        mycursor.close()
-        mydb.close()
-    except mysql.connector.Error as err:
-        print("Error:", err)
