4fcaeaadc1a9ca28508306c7565938863b87ba71
miguelperson <mi529437@ucf.edu>
2023-12-22 16:18:26 -0500

    Updated GUI for frame input

--- Server.py
+++ Server.py
@@ -42,8 +42,9 @@ def handle_client(conn, addr): # handles communication between client and server
                 file_bytes += data
             progress.update(1024) # updates the progress bar
         file.write(file_bytes) # writes the file bytes to the file variable    
-        # add line to execute sql command to insert into project table
-
+        # add line to execute sql command to insert into project table || insert_into_project(project_name, client, frames_total, start_frame, end_frame)
+        frames_total = 1 + (end_frame-start_frame) # gives us total frame value
+        insert_into_project(file_name,addr,frames_total, start_frame, end_frame)
     conn.close() # closes the connection
         
 
