8bc7f5514a82edd1eb3a991de76a2dea88bda183
miguelperson <mi529437@ucf.edu>
2024-01-11 16:48:48 -0500

    setting up send file back end

--- Client.py
+++ Client.py
@@ -27,8 +27,9 @@ def send(msg):
     client.send(message) # then we send the message
     print(client.recv(2048).decode(FORMAT)) # prints the message recieved from the server to the terminal, put 2048 because server responses are known to be short
     
-def send_file(file_location, type): # file_location stores file path
+def send_file(file_location, renderType): # file_location stores file path
     print('this is a place holder')
+    #file_info = f"{filename},{frames_total},{start_frame},{end_frame}" # going to add variables to send as overall header file
     
 def disconnect(): # called by GUI to close connection
     send("!DISCONNECT")
