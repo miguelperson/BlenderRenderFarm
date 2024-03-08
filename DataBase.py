8401031b463fa33c12eb14f93053d216468adbd6
Victoria Ten <victoria.ten.work@gmail.com>
2024-03-08 15:05:34 -0500

    r

--- DataBase.py
+++ DataBase.py
@@ -5,11 +5,12 @@ mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="7323",
-    #database="renderdb" #comment when reate database and uncomment after
+    database="renderdb" #comment when reate database and uncomment after
 )
 
 # Create a cursor object
 mycursor = mydb.cursor()
+
 """
 mycursor.execute ("DROP DATABASE renderdb")
 """
@@ -26,69 +27,14 @@ mycursor.execute("CREATE TABLE render (frame_number smallint UNSIGNED, projectID
 
 mysql
 # Create Tables - Mikaela :) :)
-
-mycursor.execute
-("
-    CREATE TABLE project 
-    (
-    projectID       INT AUTO_INCREMENT,
-    client          VARCHAR(255),  
-    project_name    VARCHAR(255), 
-    frames_total    smallint UNSIGNED, 
-    start_frame     smallint UNSIGNED, 
-    end_frame       smallint UNSIGNED, 
-    completed       ENUM('1', '2', '3'),
-
-    PRIMARY KEY projectID
-    )
-"); 
-
-mycursor.execute
-("
-    CREATE TABLE workers 
-    (
-    workerIP        VARCHAR(15),
-    available       TINYINT(1), 
-    current_project INT,
-
-    FOREIGN KEY (current_project) REFERENCES 
-    )
-")
+"""
+#mycursor.execute("CREATE TABLE project (projectID INT AUTO_INCREMENT PRIMARY KEY, client VARCHAR(255), project_name VARCHAR(255), ames_total smallint UNSIGNED, start_frame smallint UNSIGNED, end_frame smallint UNSIGNED, completed ENUM('1', '2', '3'))") 
+mycursor.execute("CREATE TABLE workers (workerIP VARCHAR(15), available TINYINT(1), current_project INT, FOREIGN KEY (current_project) REFERENCES project(projectID))")
 #TINYINT(1) = true
+mycursor.execute("CREATE TABLE render (frame_number smallint UNSIGNED, projectID INT, FOREIGN KEY (projectID) REFERENCES project(projectID))")
+mycursor.execute("CREATE TABLE performance(projectID INT, project_name VARCHAR(255), workerIP VARCHAR(15), frames_total smallint UNSIGNED, time_total VARCHAR(100), start_time VARCHAR(100), end_time VARCHAR(100), worker1_avg_time VARCHAR(100), worker2_avg_time VARCHAR(100), FOREIGN KEY (projectID) REFERENCES project(projectID), FOREIGN KEY (project_name) REFERENCES project(project_name), FOREIGN KEY (workerIP) REFERENCES workers(workerIP), FOREIGN KEY (frames_total) REFERENCES project(frames_total))")         
 
-mycursor.execute
-("
-    CREATE TABLE render 
-    (
-        frame_number    smallint UNSIGNED, 
-        projectID       INT, 
-
-        FOREIGN KEY (projectID) REFERENCES project(projectID)
-    )
-")
-
-mycursor.execute
-("
-    CREATE TABLE performance 
-    (
-        projectID        INT, 
-        project_name     VARCHAR(255), 
-        workerIP         VARCHAR(15), 
-        frames_total     smallint UNSIGNED, 
-        time_total       VARCHAR(100), 
-        start_time       VARCHAR(100), 
-        end_time         VARCHAR(100), 
-        worker1_avg_time VARCHAR(100), 
-        worker2_avg_time VARCHAR(100), 
-
-        FOREIGN KEY (projectID) REFERENCES project(projectID), 
-        FOREIGN KEY (project_name) REFERENCES project(project_name), 
-        FOREIGN KEY (worker_IP) REFERENCES workers(workerIP), 
-        FOREIGN KEY (frames_total) REFERENCES project(frames_total)
-    )
-");         
-
-
+"""
 #----------------------------------------------------------------------------------
 """
 #Hello everybody my name is Markiplier
