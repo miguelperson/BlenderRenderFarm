dfd25db0be8933cf592918d944e6b206f23dcb2c
Victoria Ten <victoria.ten.work@gmail.com>
2024-03-08 15:44:04 -0500

    d

--- DataBase.py
+++ DataBase.py
@@ -35,23 +35,22 @@ mysql
 #mycursor.execute("CREATE TABLE workers (workerIP VARCHAR(15), available TINYINT(1), current_project INT, FOREIGN KEY (current_project) REFERENCES project(projectID))")
 #TINYINT(1) = true
 #mycursor.execute("CREATE TABLE render (frame_number smallint UNSIGNED, projectID INT, FOREIGN KEY (projectID) REFERENCES project(projectID))")
-mycursor.execute("CREATE TABLE performance(projectID INT, project_name VARCHAR(255), workerIP VARCHAR(15), frames_total smallint UNSIGNED, time_total VARCHAR(100), start_time VARCHAR(100), end_time VARCHAR(100), worker1_avg_time VARCHAR(100), worker2_avg_time VARCHAR(100))")         
+#mycursor.execute("CREATE TABLE performance(projectID INT, project_name VARCHAR(255), workerIP VARCHAR(15), frames_total smallint UNSIGNED, time_total VARCHAR(100), start_time VARCHAR(100), end_time VARCHAR(100), worker1_avg_time VARCHAR(100), worker2_avg_time VARCHAR(100))")         
 
 """
 #----------------------------------------------------------------------------------
 """
 #Hello everybody my name is Markiplier
 """
-mycursor.execute("SHOW TABLES")s
+mycursor.execute("SHOW TABLES")
 
 for x in mycursor:
   print(x)
 
-mycursor.execute("DESCRIBE render")
+mycursor.execute("DESCRIBE performance")
 result = mycursor.fetchall()
 for row in result:
     print(row)
-
 """
 """
 mycursor.execute("SELECT * FROM project")
