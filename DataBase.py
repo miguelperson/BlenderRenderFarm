d2bce5b40d761b1c64f9326cd4806bff2842cd92
micmickaay <your.email@example.com>
2024-03-08 14:23:55 -0500

    Mikaela Tables

--- DataBase.py
+++ DataBase.py
@@ -23,6 +23,72 @@ mycursor.execute("CREATE DATABASE renderdb")
 mycursor.execute("CREATE TABLE project (projectID INT AUTO_INCREMENT PRIMARY KEY, project_name VARCHAR(255), client VARCHAR(100), frames_total smallint UNSIGNED, start_frame smallint UNSIGNED, end_frame smallint UNSIGNED)")
 mycursor.execute("CREATE TABLE workers (worker VARCHAR(100), available TINYINT(1), projectID INT, FOREIGN KEY (projectID) REFERENCES project(projectID))") #TINYINT(1)=true
 mycursor.execute("CREATE TABLE render (frame_number smallint UNSIGNED, projectID INT, FOREIGN KEY (projectID) REFERENCES project(projectID))")
+
+mysql
+# Create Tables - Mikaela :) :)
+
+mycursor.execute
+("
+    CREATE TABLE project 
+    (
+    projectID       INT AUTO_INCREMENT,
+    client          VARCHAR(255),  
+    project_name    VARCHAR(255), 
+    frames_total    smallint UNSIGNED, 
+    start_frame     smallint UNSIGNED, 
+    end_frame       smallint UNSIGNED, 
+    completed       ENUM('1', '2', '3'),
+
+    PRIMARY KEY projectID
+    )
+"); 
+
+mycursor.execute
+("
+    CREATE TABLE workers 
+    (
+    workerIP        VARCHAR(15),
+    available       TINYINT(1), 
+    current_project INT,
+
+    FOREIGN KEY (current_project) REFERENCES 
+    )
+")
+#TINYINT(1) = true
+
+mycursor.execute
+("
+    CREATE TABLE render 
+    (
+        frame_number    smallint UNSIGNED, 
+        projectID       INT, 
+
+        FOREIGN KEY (projectID) REFERENCES project(projectID)
+    )
+")
+
+mycursor.execute
+("
+    CREATE TABLE performance 
+    (
+        projectID        INT, 
+        project_name     VARCHAR(255), 
+        workerIP         VARCHAR(15), 
+        frames_total     smallint UNSIGNED, 
+        time_total       VARCHAR(100), 
+        start_time       VARCHAR(100), 
+        end_time         VARCHAR(100), 
+        worker1_avg_time VARCHAR(100), 
+        worker2_avg_time VARCHAR(100), 
+
+        FOREIGN KEY (projectID) REFERENCES project(projectID), 
+        FOREIGN KEY (project_name) REFERENCES project(project_name), 
+        FOREIGN KEY (worker_IP) REFERENCES workers(workerIP), 
+        FOREIGN KEY (frames_total) REFERENCES project(frames_total)
+    )
+");         
+
+
 #----------------------------------------------------------------------------------
 """
 #Hello everybody my name is Markiplier
